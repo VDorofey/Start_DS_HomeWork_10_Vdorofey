10f6f3082778c4cbbc09cae2e73b801a99a07e45
VDorofey <163355396+VDorofey@users.noreply.github.com>
2024-04-01 00:12:20 +0300

    Update app_IndianPatient.py

--- app_IndianPatient.py
+++ app_IndianPatient.py
@@ -64,44 +64,44 @@ def process_side_bar_inputs():
     user_input_df = sidebar_input_features()
 
     
-def sidebar_input_features():
-    Gender = st.sidebar.selectbox("Пол", ("Мужской", "Женский"))
-   
-    # Age = st.sidebar.slider("Возраст", min_value=Age_min, max_value=Age_max, value=Age_min + 20, step=1)
 
-    Total_Bilirubin = st.sidebar.slider("Общий билирубин",
+Gender = st.sidebar.selectbox("Пол", ("Мужской", "Женский"))
+   
+Age = st.sidebar.slider("Возраст", min_value=Age_min, max_value=Age_max, value=Age_min + 20, step=1)
+
+Total_Bilirubin = st.sidebar.slider("Общий билирубин",
         min_value=Total_Bilirubin_min, max_value=Total_Bilirubin_max, value=Total_Bilirubin_min + 1.0, step=0.1)
 
-    Direct_Bilirubin = st.sidebar.slider("Прямой билирубин",
+Direct_Bilirubin = st.sidebar.slider("Прямой билирубин",
         min_value=Direct_Bilirubin_min, max_value=Direct_Bilirubin_max, value=Direct_Bilirubin_min + 0.5, step=0.1)
 
-    Alkaline_Phosphotase = st.sidebar.slider("Щелочная фосфотаза",
+Alkaline_Phosphotase = st.sidebar.slider("Щелочная фосфотаза",
         min_value=Alkaline_Phosphotase_min, max_value=Alkaline_Phosphotase_max, value=Alkaline_Phosphotase_min + 70, step=1)
 
-    Alamine_Aminotransferase = st.sidebar.slider("АЛТ",
+Alamine_Aminotransferase = st.sidebar.slider("АЛТ",
         min_value=Alamine_Aminotransferase_min, max_value=Alamine_Aminotransferase_max, value=Alamine_Aminotransferase_min + 20, step=1)
     
-    Aspartate_Aminotransferase = st.sidebar.slider("АСТ",
+Aspartate_Aminotransferase = st.sidebar.slider("АСТ",
         min_value=Aspartate_Aminotransferase_min, max_value=Aspartate_Aminotransferase_max, value=Aspartate_Aminotransferase_min + 20, step=1)
 
 
-    Total_Protiens = st.sidebar.slider("Общий белок",
+Total_Protiens = st.sidebar.slider("Общий белок",
         min_value=Total_Protiens_min, max_value=Total_Protiens_max, value=Total_Protiens_min + 3.0, step=0.1)
 
-    Albumin = st.sidebar.slider(
+Albumin = st.sidebar.slider(
         "Альбумин",
         min_value=Albumin_min, max_value=Albumin_max, value=Albumin_min + 1.0, step=0.1)
 
-    Albumin_and_Globulin_Ratio = st.sidebar.slider(
+Albumin_and_Globulin_Ratio = st.sidebar.slider(
         "Соотношение Альбумин/Глобулин",
         min_value=Albumin_and_Globulin_Ratio_min, max_value=Albumin_and_Globulin_Ratio_max, value=Albumin_and_Globulin_Ratio_min + 0.5, step=0.1)
 
-    translatetion = {
+translatetion = {
         "Мужской": "Муж",
         "Женский": "Жен",
          }
 
-    data = {
+data = {
         "Возраст": Age,
         "Пол": translatetion[Gender],
         "Общ.билирубин": Total_Bilirubin,
@@ -114,16 +114,14 @@ def sidebar_input_features():
         "Альбумин/Глобулин": Albumin_and_Globulin_Ratio,
     }
 
-    df = pd.DataFrame(data, index=[0])
-    st.dataframe(df)
+df = pd.DataFrame(data, index=[0])
+st.dataframe(df)
 
-    st.write("# Заключение врача:")
-    if (Age >= 50) or (Total_Bilirubin > 45) or (Direct_Bilirubin > 10) or (Alkaline_Phosphotase > 1000) or (Alamine_Aminotransferase >1000) or   (Aspartate_Aminotransferase > 1000) or (Total_Protiens > 5.8) or (Albumin > 2.75) or (Albumin_and_Globulin_Ratio > 1.4):
+st.write("# Заключение врача:")
+if (Age >= 50) or (Total_Bilirubin > 45) or (Direct_Bilirubin > 10) or (Alkaline_Phosphotase > 1000) or (Alamine_Aminotransferase >1000) or   (Aspartate_Aminotransferase > 1000) or (Total_Protiens > 5.8) or (Albumin > 2.75) or (Albumin_and_Globulin_Ratio > 1.4):
         st.write("Пациент скорее болен, чем здоров")
-    else:
+else:
         st.write("Пациент скорее здоров, чем болен")
-
-    return df
 
 if __name__ == "__main__":
     process_main_page()
